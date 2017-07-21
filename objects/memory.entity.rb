99b65263fc7bbe18141594584fe51b99e5c8b2b1
Jake Franklin <jacob.d.franklin@gmail.com>
2017-07-20 23:14:04 -0700

    Error fixes to adding entities to memories

--- objects/memory.entity.rb
+++ objects/memory.entity.rb
@@ -4,16 +4,19 @@ class EntityMemory < Memory_Array
 
     def add entity
 
+        if !entity.is_a? Entity then return end
+
         new_id      = self.length.to_s.prepend("0", 5)
-        parent_id   = entity.parent ? entity.parent.id : 'NULL0'
+        parent_id   = (entity.parent && !entity.parent.id.nil?) ? entity.parent.id : 'NULL0'
         type        = entity.type.append(" ", 10)
         name        = entity.name.append(" ", 14)
+        preposition = entity.preposition.append(" ", 50)
         adjective   = entity.adjective.append(" ", 14)
         verb        = entity.verb.append(" ", 20)
         adverb      = entity.adverb.append(" ", 14)
         # lang_id     = entity.lang_id
 
-        self.append("#{new_id} #{parent_id.append(" ", 9)} #{type} #{name} #{adjective} #{verb} #{adverb}")
+        self.append("#{new_id} #{parent_id.append(" ", 9)} #{type} #{name} #{preposition} #{adjective} #{verb} #{adverb}")
 
     end
 
