fb48580166ffb96124726bb70e8de9d335101333
Jake Franklin <jacob.d.franklin@gmail.com>
2021-06-13 21:53:39 -0700

    first functional tool for guided temaplate authoring

--- actions/action.listen.rb
+++ actions/action.listen.rb
@@ -17,10 +17,39 @@ class ActionListen
 
     end
 
+    # Ideas:
+    # - What would you like to work on? (summaries, arcs, scenes)
+    #   after selecting one, then you can either pick a parent and
+    #   work within it, or you can pick a mode where you just write
+    #   for the summary or arc with the fewest children.
     def act q = nil
 
-        @arcs_by_summary.each_pair {|k, v| puts "Summary ID #{k} has #{v.length} arcs"}
+        lowest_arcs = 99
+        lowest_arcs_summary_id = nil
+        lowest_arcs_summary = nil
+        @arcs_by_summary.each_pair do |k, v|
+            if v.length < lowest_arcs then
+                lowest_arcs = v.length
+                lowest_arcs_summary_id = k
+            end
+        end
 
+        lowest_arcs_summary = $summaries
+            .to_a
+            .select {|summary| summary['id'] == lowest_arcs_summary_id }
+            .first
+
+        puts "Write an arc for summary ID #{lowest_arcs_summary_id} (it has only #{lowest_arcs} arcs)"
+        puts "Summary: #{lowest_arcs_summary['summary']}"
+        puts "Current arcs: #{$arcs.get_by_summary_id(lowest_arcs_summary_id).map{|arc| [arc.order, arc.text] }}"
+        puts "Input arc text:"
+        arc = STDIN.gets.chomp
+        puts "What order does this arc have? (number, can be comma seperated)"
+        order = STDIN.gets.chomp
+        puts "Adding arc: #{arc}, order: #{order}, for summary ID: #{lowest_arcs_summary_id}"
+        new_arc_id = $arcs.add(lowest_arcs_summary_id, order, arc)
+        puts "done.\n\n"
+        puts "Now, write some scenes for that new arc (arc ID #{new_arc_id}) "
 
     end
 
