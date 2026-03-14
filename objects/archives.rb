d90b43d49128cdb72da3d5b7fe070aab9ba4cdc3
Jake Franklin <jacob.d.franklin@gmail.com>
2026-03-14 08:24:10 -0700

    Lint/format objects

--- objects/archives.rb
+++ objects/archives.rb
@@ -1,32 +1,36 @@
-# Yuri gazes into the fire, as he searches the vast archives of his mind for...
-require_relative "./entity"
+require_relative './entity'
 
+# Yuri gazes into the fire, as he searches the vast archives of his mind for...
 class Archives
 
-    def initialize host
+  def initialize(host)
 
-        @entity_types = {}
-        Dir.entries("#{host.path}/memory/world_generation/entity_types").each do |file|
-            if file == "." || file == ".." then next end
-            name = file.sub(".mh", "")
-            @entity_types[name.to_sym] = Memory_Hash.new("world_generation/entity_types/#{name}", host.path).to_h
-        end
+    @entity_types = {}
+    Dir.entries("#{host.path}/memory/world_generation/entity_types").each do |file|
 
-    end
+      next if ['.', '..'].include?(file)
 
-    def create type, options
-
-        raise "#{type} is not a valid entity type. You should create one. Valid entity types are #{@entity_types}" unless @entity_types[type]
-
-        return Entity.new(@entity_types[type], options)
+      name = file.sub('.mh', '')
+      @entity_types[name.to_sym] = Memory_Hash.new("world_generation/entity_types/#{name}", host.path).to_h
 
     end
 
-    def get attributes, options = {}
+  end
 
-        return Entity.new(attributes, options)
+  def create(type, options)
 
+    unless @entity_types[type]
+      raise "#{type} is not a valid entity type. You should create one. Valid entity types are #{@entity_types}"
     end
 
+    Entity.new(@entity_types[type], options)
+
+  end
+
+  def get(attributes, options = {})
+
+    Entity.new(attributes, options)
+
+  end
 
 end
