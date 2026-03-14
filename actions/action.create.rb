b4c906ced71aeae52940da4617659c82360259c0
Jake Franklin <jacob.d.franklin@gmail.com>
2026-03-14 08:21:58 -0700

    Lint/format actions

--- actions/action.create.rb
+++ actions/action.create.rb
@@ -1,121 +1,129 @@
-require_relative "../objects/memory.arc"
+require_relative '../objects/memory.arc'
 
+# Create a new story template component
 class ActionCreate
 
-    include Action
+  include Action
 
-    @@indent = "\s" * 4
-    @@entity_type_num_args = 7
-    @@entity_type_template = "" \
-        "TYPE : %s\n" \
-        "CMAX : %s\n\n" \
-        "ADJV\n#{@@indent}%s\n\n" \
-        "VERB\n#{@@indent}%s\n\n" \
-        "ADVB\n#{@@indent}%s\n\n" \
-        "CTPS\n#{@@indent}%s\n\n" \
-        "CPRP\n#{@@indent}%s\n\n" \
+  INDENT_AMOUT = "\s" * 4
+  ENTITY_TYPE_NUM_ARGS = 7
+  ENTITY_TYPE_TEMPLATE = '' \
+      "TYPE : %s\n" \
+      "CMAX : %s\n\n" \
+      "ADJV\n#{INDENT_AMOUT}%s\n\n" \
+      "VERB\n#{INDENT_AMOUT}%s\n\n" \
+      "ADVB\n#{INDENT_AMOUT}%s\n\n" \
+      "CTPS\n#{INDENT_AMOUT}%s\n\n" \
+      "CPRP\n#{INDENT_AMOUT}%s\n\n".freeze
 
 
-    def initialize q = nil
+  def initialize(q = nil)
 
-        super
+    super
 
-        @name = "Create"
-        @docs = "Create new memories. Current commands: `create entity_type`, `create arc`, `create scene`"
+    @name = 'Create'
+    @docs = 'Create new memories. Current commands: `create entity_type`, `create arc`, `create scene`'
+
+  end
+
+  def act(q = nil)
+
+    raise 'You must specify what you want to create...' if q.nil?
+
+    $tags ||= Memory_Array.new('story_templates/tags', @host.path)
+    $summaries ||= Memory_Array.new('story_templates/summaries', @host.path)
+    $arcs ||= ArcMemory.new('story_templates/arcs', @host.path)
+    $scenes ||= SceneMemory.new('story_templates/scenes', @host.path)
+
+    args = q.split(' ')
+    case args.first
+    when 'entity_type'
+      unless args.length > 1
+        raise 'You must at least specify the type of entity you are wanting to create e.g., `create entity_type Water`'
+      end
+
+      create_entity_type(args[1..])
+    when 'arc'
+      start_arc_creator
+    when 'scene'
+      start_scene_creator
+    else
+      puts "\"#{args.first}\" is not something #{@host.name} can create right now."
+    end
+
+  end
+
+  private
+
+  def create_entity_type(attrs = [])
+
+    ENTITY_TYPE_NUM_ARGS.times do |i|
+
+      next if attrs[i]
+
+      attrs[i] = ''
 
     end
 
-    def act q = nil
+    new_file = File.new("#{@host.path}/memory/world_generation/entity_types/#{attrs.first.downcase}.mh", 'w')
+    new_file.puts(ENTITY_TYPE_TEMPLATE % attrs)
+    new_file.close
 
-        raise "You must specify what you want to create..." unless !q.nil?
+    puts ENTITY_TYPE_TEMPLATE % attrs
 
-        $tags ||= Memory_Array.new("story_templates/tags", @host.path)
-        $summaries ||= Memory_Array.new("story_templates/summaries", @host.path)
-        $arcs ||= ArcMemory.new("story_templates/arcs", @host.path)
-        $scenes ||= SceneMemory.new("story_templates/scenes", @host.path)
+  end
 
-        args = q.split(' ')
-        case args.first
-        when 'entity_type'
-            raise "You must at least specify the type of entity you are wanting to create e.g., `create entity_type Water`" unless args.length > 1
-            create_entity_type(args[1..-1])
-        when 'arc'
-            start_arc_creator
-        when 'scene'
-            start_scene_creator
-        else
-            puts "\"#{args.first}\" is not something #{@host.name} can create right now."
-        end
+  def start_arc_creator
 
-    end
+    puts 'Create arc for which summary template? (enter ID)'
+    $summaries.to_a.each { |s| puts "#{s['id']} : #{s['summary']}" }
+    summary_id = STDIN.gets.chomp
+    summary = $summaries.filter('id', summary_id.prepend('0', 5), nil).first
+    puts "Creating arc for summary #{summary['summary']}"
+    puts 'Here are the arcs for the selected summary:'
+    $arcs.get_by_summary_id(summary_id).each { |a| puts "#{a.text}, order: #{a.order}" }
+    puts 'Input arc text:'
+    arc = STDIN.gets.chomp
+    puts 'What order does this arc have? (number, can be comma seperated)'
+    order = STDIN.gets.chomp
+    puts "Adding arc: #{arc}, order: #{order}, for summary ID: #{summary_id}"
+    $arcs.add(summary_id, order, arc)
+    puts 'done.'
 
-    private
+  end
 
-    def create_entity_type attrs = []
+  def start_scene_creator
 
-        @@entity_type_num_args.times do |i|
-            next if attrs[i]
-            attrs[i] = ''
-        end
+    puts 'What arc would you like to create a scene for? Enter the ID:'
+    $summaries.to_a.each do |s|
 
-        new_file = File.new("#{@host.path}/memory/world_generation/entity_types/#{attrs.first.downcase}.mh", "w")
-        new_file.puts(@@entity_type_template % attrs)
-        new_file.close
+      summary_id = s['id']
 
-        puts @@entity_type_template % attrs
+      puts "Story: #{s['summary']}"
+      $arcs.get_by_summary_id(summary_id).each { |a| puts "    #{a.id} - #{a.text}, order: #{a.order}" }
 
     end
+    arc_id = STDIN.gets.chomp
+    arc = $arcs.get(arc_id)
+    puts "Creating scene for arc #{arc_id}, '#{arc.text}'"
+    puts "\n"
+    puts 'Existing scenes for selected arc:'
+    $scenes.get_by_arc_id(arc_id).each { |s| puts " - #{s.time} #{s.setting} #{s.action}" }
+    puts "\n"
+    puts "The following tags can be used (use the 'tag' exactly as written (including carats <>)):"
+    $tags.to_a.each { |t| puts "Tag: #{t['tag']}, Type: #{t['entity']}" }
+    puts 'Input TIME description:'
+    time = STDIN.gets.chomp
+    puts 'Input ACTION description:'
+    action = STDIN.gets.chomp
+    puts 'Input SETTING description:'
+    setting = STDIN.gets.chomp
+    puts 'Input ORDER this scene should appear in (can be comma separated):'
+    order = STDIN.gets.chomp
+    puts 'Creating scene...'
+    $scenes.add(arc_id, time, action, setting, order)
+    puts 'Scene created.'
 
-    def start_arc_creator
-
-        summary_id = "1"
-        puts "Create arc for which summary template? (enter ID)"
-        $summaries.to_a.each {|s| puts "#{s["id"]} : #{s["summary"]}" }
-        summary_id = STDIN.gets.chomp
-        summary = $summaries.filter("id", summary_id.prepend("0", 5), nil).first
-        puts "Creating arc for summary #{summary["summary"]}"
-        puts "Here are the arcs for the selected summary:"
-        $arcs.get_by_summary_id(summary_id).each{|a| puts "#{a.text}, order: #{a.order}"}
-        puts "Input arc text:"
-        arc = STDIN.gets.chomp
-        puts "What order does this arc have? (number, can be comma seperated)"
-        order = STDIN.gets.chomp
-        puts "Adding arc: #{arc}, order: #{order}, for summary ID: #{summary_id}"
-        $arcs.add(summary_id, order, arc)
-        puts "done."
-
-    end
-
-    def start_scene_creator
-
-        puts "What arc would you like to create a scene for? Enter the ID:"
-        $summaries.to_a.each do |s|
-            summary_id = s["id"]
-
-            puts "Story: #{s["summary"]}"
-            $arcs.get_by_summary_id(summary_id).each{|a| puts "    #{a.id} - #{a.text}, order: #{a.order}"}
-        end
-        arc_id = STDIN.gets.chomp
-        arc = $arcs.get(arc_id)
-        puts "Creating scene for arc #{arc_id}, '#{arc.text}'"
-        puts "\n"
-        puts "Existing scenes for selected arc:"
-        $scenes.get_by_arc_id(arc_id).each{|s| puts " - #{s.time} #{s.setting} #{s.action}"}
-        puts "\n"
-        puts "The following tags can be used (use the 'tag' exactly as written (including carats <>)):"
-        $tags.to_a.each{|t| puts "Tag: #{t["tag"]}, Type: #{t["entity"]}"}
-        puts "Input TIME description:"
-        time = STDIN.gets.chomp
-        puts "Input ACTION description:"
-        action = STDIN.gets.chomp
-        puts "Input SETTING description:"
-        setting = STDIN.gets.chomp
-        puts "Input ORDER this scene should appear in (can be comma separated):"
-        order = STDIN.gets.chomp
-        puts "Creating scene..."
-        $scenes.add(arc_id, time, action, setting, order)
-        puts "Scene created."
-
-    end
+  end
 
 end
--- actions/action.listen.rb
+++ actions/action.listen.rb
@@ -1,139 +1,151 @@
+# Interactively write more story templates
 class ActionListen
 
-    include Action
+  include Action
 
-    def initialize q = nil
+  def initialize(q = nil)
 
-        super
+    super
 
-        @name = "Listen"
-        @docs = "Yuri can listen to the untold. Tell him. An interactive tool for writing missing scenes and arcs."
+    @name = 'Listen'
+    @docs = 'Yuri can listen to the untold. Tell him. An interactive tool for writing missing scenes and arcs.'
 
-        $tags ||= Memory_Array.new("story_templates/tags", @host.path)
-        $summaries ||= Memory_Array.new("story_templates/summaries", @host.path)
-        $arcs ||= ArcMemory.new("story_templates/arcs", @host.path)
-        $scenes ||= SceneMemory.new("story_templates/scenes", @host.path)
+    $tags ||= Memory_Array.new('story_templates/tags', @host.path)
+    $summaries ||= Memory_Array.new('story_templates/summaries', @host.path)
+    $arcs ||= ArcMemory.new('story_templates/arcs', @host.path)
+    $scenes ||= SceneMemory.new('story_templates/scenes', @host.path)
 
-        @arcs_by_summary = $summaries.to_a.inject({}) do |memo, summary|
-            s_id = summary["id"]
-            summary_arcs = $arcs.get_by_summary_id(s_id)
-            memo.update(s_id => summary_arcs)
-        end
+    @arcs_by_summary = $summaries.to_a.inject({}) do |memo, summary|
 
-        @scenes_by_arc = $arcs.to_a.inject({}) do |memo, arc|
-            arc_id = arc["id"]
-            arc_scenes = $scenes.get_by_arc_id(arc_id)
-            memo.update(arc_id => arc_scenes)
-        end
+      s_id = summary['id']
+      summary_arcs = $arcs.get_by_summary_id(s_id)
+      memo.update(s_id => summary_arcs)
 
     end
 
-    # Ideas:
-    # - What would you like to work on? (summaries, arcs, scenes)
-    #   after selecting one, then you can either pick a parent and
-    #   work within it, or you can pick a mode where you just write
-    #   for the summary or arc with the fewest children.
-    def act q = nil
+    @scenes_by_arc = $arcs.to_a.inject({}) do |memo, arc|
 
-        if !q.nil? && q != "" then
-            topic = q
-        else
-            puts "What would you like to work on?"
-            puts "(summaries, arcs, or scenes)"
-            topic = STDIN.gets.chomp
-        end
-
-        case topic
-        when "arcs"
-            listen_arcs
-        when "scenes"
-            listen_scenes
-        else
-            puts "#{topic} is not a valid option"
-            return nil
-        end
+      arc_id = arc['id']
+      arc_scenes = $scenes.get_by_arc_id(arc_id)
+      memo.update(arc_id => arc_scenes)
 
     end
 
-    def listen_arcs
+  end
 
-        lowest_arcs = 99
-        lowest_arcs_summary_id = nil
-        lowest_arcs_summary = nil
-        @arcs_by_summary.each_pair do |k, v|
-            if v.length < lowest_arcs then
-                lowest_arcs = v.length
-                lowest_arcs_summary_id = k
-            end
-        end
+  # Ideas:
+  # - What would you like to work on? (summaries, arcs, scenes)
+  #   after selecting one, then you can either pick a parent and
+  #   work within it, or you can pick a mode where you just write
+  #   for the summary or arc with the fewest children.
+  def act(q = nil)
 
-        lowest_arcs_summary = $summaries
-            .to_a
-            .select {|summary| summary['id'] == lowest_arcs_summary_id }
-            .first
+    if !q.nil? && q != ''
+      topic = q
+    else
+      puts 'What would you like to work on?'
+      puts '(summaries, arcs, or scenes)'
+      topic = STDIN.gets.chomp
+    end
 
-        puts "Write an arc for summary ID #{lowest_arcs_summary_id} (it has only #{lowest_arcs} arcs)"
-        puts "Summary: #{lowest_arcs_summary['summary']}"
-        puts "Current arcs: #{$arcs.get_by_summary_id(lowest_arcs_summary_id).map{|arc| [arc.order, arc.text] }}"
-        puts "Input arc text:"
-        arc = STDIN.gets.chomp
-        puts "What order does this arc have? (number, can be comma seperated)"
-        order = STDIN.gets.chomp
-        puts "Adding arc: #{arc}, order: #{order}, for summary ID: #{lowest_arcs_summary_id}"
-        new_arc_id = $arcs.add(lowest_arcs_summary_id, order, arc)
-        puts "done.\n\n"
-        puts "Now, write some scenes for that new arc (arc ID #{new_arc_id}) "
+    case topic
+    when 'arcs'
+      listen_arcs
+    when 'scenes'
+      listen_scenes
+    else
+      puts "#{topic} is not a valid option"
+      nil
+    end
+
+  end
+
+  def listen_arcs
+
+    lowest_arcs = 99
+    lowest_arcs_summary_id = nil
+    @arcs_by_summary.each_pair do |k, v|
+
+      if v.length < lowest_arcs
+        lowest_arcs = v.length
+        lowest_arcs_summary_id = k
+      end
 
     end
 
-    def listen_scenes
+    lowest_arcs_summary = $summaries
+                          .to_a
+                          .select { |summary| summary['id'] == lowest_arcs_summary_id }
+                          .first
 
-        lowest_scenes = 999
-        lowest_scenes_arc_id = nil
-        lowest_scenes_arc = nil
-        lowest_scenes_arc_summary = nil
-        @scenes_by_arc.each_pair do |k, v|
-            if v.length < lowest_scenes then
-                lowest_scenes = v.length
-                lowest_scenes_arc_id = k
-            end
-        end
+    puts "Write an arc for summary ID #{lowest_arcs_summary_id} (it has only #{lowest_arcs} arcs)"
+    puts "Summary: #{lowest_arcs_summary['summary']}"
+    puts "Current arcs: #{$arcs.get_by_summary_id(lowest_arcs_summary_id).map { |arc| [arc.order, arc.text] }}"
+    puts 'Input arc text:'
+    arc = STDIN.gets.chomp
+    puts 'What order does this arc have? (number, can be comma seperated)'
+    order = STDIN.gets.chomp
+    puts "Adding arc: #{arc}, order: #{order}, for summary ID: #{lowest_arcs_summary_id}"
+    new_arc_id = $arcs.add(lowest_arcs_summary_id, order, arc)
+    puts "done.\n\n"
+    puts "Now, write some scenes for that new arc (arc ID #{new_arc_id}) "
 
-        lowest_scenes_arc = $arcs
-            .to_a
-            .select {|arc| arc['id'] == lowest_scenes_arc_id }
-            .first
-        lowest_scenes_arc_summary = $summaries
-            .to_a
-            .select {|summary| summary["id"] == lowest_scenes_arc["summary_id"] }
-            .first
+  end
 
-        puts "Write a scene for arc ID #{lowest_scenes_arc_id} (it has only #{lowest_scenes} scenes)"
+  def listen_scenes
 
-        continue = 'y'
-        while continue
-            puts "Story Summary: #{lowest_scenes_arc_summary['summary']}"
-            puts "Arc Text: #{lowest_scenes_arc['text']}"
-            puts "Current scenes: #{$scenes.get_by_arc_id(lowest_scenes_arc_id).map{|scene| [scene.order, scene.describe] }}"
-            puts "The following tags can be used (use the 'tag' exactly as written (including carats <>)):"
-            $tags.to_a.each{|t| puts "Tag: #{t["tag"]}, Type: #{t["entity"]}"}
-            puts "Input TIME description:"
-            time = STDIN.gets.chomp
-            puts "Input ACTION description:"
-            action = STDIN.gets.chomp
-            puts "Input SETTING description:"
-            setting = STDIN.gets.chomp
-            puts "Input ORDER this scene should appear in (can be comma separated):"
-            order = STDIN.gets.chomp
-            puts "Creating scene..."
-            $scenes.add(lowest_scenes_arc_id, time, action, setting, order)
-            puts "Scene created."
-            puts "Continue creating scenes for arc #{lowest_scenes_arc_id}? (y/n)"
+    lowest_scenes = 999
+    lowest_scenes_arc_id = nil
+    lowest_scenes_arc = nil
+    @scenes_by_arc.each_pair do |k, v|
 
-            continue = STDIN.gets.chomp
-
-            break unless continue == 'y'
-        end
+      if v.length < lowest_scenes
+        lowest_scenes = v.length
+        lowest_scenes_arc_id = k
+      end
 
     end
+
+    lowest_scenes_arc = $arcs
+                        .to_a
+                        .select { |arc| arc['id'] == lowest_scenes_arc_id }
+                        .first
+    lowest_scenes_arc_summary = $summaries
+                                .to_a
+                                .select { |summary| summary['id'] == lowest_scenes_arc['summary_id'] }
+                                .first
+
+    puts "Write a scene for arc ID #{lowest_scenes_arc_id} (it has only #{lowest_scenes} scenes)"
+
+    continue = 'y'
+    while continue
+      puts "Story Summary: #{lowest_scenes_arc_summary['summary']}"
+      puts "Arc Text: #{lowest_scenes_arc['text']}"
+      puts "Current scenes: #{$scenes.get_by_arc_id(lowest_scenes_arc_id).map do |scene|
+
+                                [scene.order, scene.describe]
+
+                              end}"
+      puts "The following tags can be used (use the 'tag' exactly as written (including carats <>)):"
+      $tags.to_a.each { |t| puts "Tag: #{t['tag']}, Type: #{t['entity']}" }
+      puts 'Input TIME description:'
+      time = STDIN.gets.chomp
+      puts 'Input ACTION description:'
+      action = STDIN.gets.chomp
+      puts 'Input SETTING description:'
+      setting = STDIN.gets.chomp
+      puts 'Input ORDER this scene should appear in (can be comma separated):'
+      order = STDIN.gets.chomp
+      puts 'Creating scene...'
+      $scenes.add(lowest_scenes_arc_id, time, action, setting, order)
+      puts 'Scene created.'
+      puts "Continue creating scenes for arc #{lowest_scenes_arc_id}? (y/n)"
+
+      continue = STDIN.gets.chomp
+
+      break unless continue == 'y'
+    end
+
+  end
+
 end
